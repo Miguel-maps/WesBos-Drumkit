7e2601562437418504d3f984f7eafcb067ed3c3e
miguel-maps <77571745+Miguel-maps@users.noreply.github.com>
2023-02-14 16:17:33 +0100

    Code cleanup, but also revert changes to responsive display

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function soundCheck(audio) {
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-}
+// Play sound by using mouse or touchpad
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    playSoundButton(button);
+  });
+});
 
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -12,6 +14,11 @@ function playSound(e) {
   audio.play();
   key.classList.add('playing');
 }
+
+// Play sound by using keyboard
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
 
 function playSoundButton(button) {
   const audio = document.querySelector(`audio[data-key="${button.getAttribute('data-key')}"]`);
@@ -26,16 +33,3 @@ function removeTransition(e) {
   if(e.propertyName !== 'transform') return;
   this.classList.remove('playing');
 }
-
-// Play sound by using keyboard
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
-
-// Play sound by using mouse or touchpad
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    playSoundButton(button);
-  });
-});
