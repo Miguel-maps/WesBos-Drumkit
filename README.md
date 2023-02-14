f8f5dd17677e61735359abc724a66475c69e932e
miguel-maps <77571745+Miguel-maps@users.noreply.github.com>
2023-02-14 15:27:15 +0100

    Play sound by also clicking the buttons

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # WesBos-Drumkit
 Coded along the WesBos tutorial, this project is a simple exercise to try event listeners in JavaScript.
+
+Improvements:
+- Make the site responsive
+    · Play sound by also clicking the buttons
+
+TODO:
+- Change styles of entire site
+    · Blur background image
+    · Change colors
+    · Reduce image size
+- Change favicon
--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+function soundCheck(audio) {
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+}
+
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing')
-  }
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  soundCheck(audio);
+  key.classList.add('playing')
+}
 
-  function removeTransition(e) {
-    if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
-  }
+function playSoundButton(button) {
+  const audio = document.querySelector(`audio[data-key="${button.getAttribute('data-key')}"]`);
+  soundCheck(audio);
+  button.classList.add('playing');
+  button.blur();
+}
 
-  const keys = document.querySelectorAll('.key');
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound)
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+// Play sound by using keyboard
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+// Play sound by using mouse or touchpad
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    playSoundButton(button);
+  });
+});
